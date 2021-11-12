6a46b5b95e7fa0c50c6012b0a706da69cd4c91f7
Mykyta_Varenyk <Mykyta_Varenyk@epam.com>
2021-11-12 16:10:19 +0200

    Delete unnecessary assert

--- src/test/java/org/folio/rest/impl/PatronResourceImplTest.java
+++ src/test/java/org/folio/rest/impl/PatronResourceImplTest.java
@@ -1333,8 +1333,6 @@ public class PatronResourceImplTest {
           actualHold.getInteger("requestPosition"));
       assertEquals(expectedHold.getString("patronComments"),
           actualHold.getString("patronComments"));
-      assertEquals(expectedHold.getString("requestLevel"),
-        actualHold.getString("requestLevel"));
       return verifyItem(expectedHold.getJsonObject("item"), actualHold.getJsonObject("item"));
     }
     return false;
