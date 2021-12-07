328dc4eda550a9f34255519c91c710c670217ce6
Mykyta_Varenyk <Mykyta_Varenyk@epam.com>
2021-12-07 14:43:30 +0200

    MODPATRON-91 Make ValidationException errors field transient

--- src/main/java/org/folio/patron/rest/exceptions/ValidationException.java
+++ src/main/java/org/folio/patron/rest/exceptions/ValidationException.java
@@ -3,7 +3,7 @@ package org.folio.patron.rest.exceptions;
 import org.folio.rest.jaxrs.model.Errors;
 
 public class ValidationException extends RuntimeException {
-  private final Errors errors;
+  private final transient Errors errors;
 
   public ValidationException(Errors errors) {
     this.errors = errors;
