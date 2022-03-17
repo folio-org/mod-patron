a0fe0d2bb964f996e723bbf4655d9571456c7551
Yusuf Murodov <yusuf_murodov@epam.com>
2022-03-17 20:51:16 +0500

    MODPATRON-22: add unit tests

---
+++ src/test/java/org/folio/model/HoldRequestStatusTest.java
@@ -0,0 +1,31 @@
+package org.folio.model;
+
+import static org.folio.rest.jaxrs.model.Hold.Status.fromValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.folio.rest.jaxrs.model.Hold;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class HoldRequestStatusTest {
+
+  @ParameterizedTest
+  @ValueSource(strings = {"Open - Not yet filled", "Open - Awaiting pickup",
+    "Open - Awaiting delivery", "Open - In transit", "Closed - Filled",
+    "Closed - Cancelled", "Closed - Unfilled", "Closed - Pickup expired"
+  })
+
+  void testValidHoldRequestStatus(String status) {
+    Hold.Status requestStatus = fromValue(status);
+    assertNotNull(requestStatus);
+    assertEquals(status, requestStatus.value());
+  }
+
+  @Test
+  void testInValidRequestStatus() {
+    assertThrows(IllegalArgumentException.class, () -> fromValue("Invalid Status"));
+  }
+}
