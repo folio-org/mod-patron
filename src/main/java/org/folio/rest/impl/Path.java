34f72d0e770bf30fd5466e0e4bba69e7b209aa46
Antony_Hruschev <anton.gitgit@gmail.com>
2024-10-04 10:31:28 +0400

    MODPATRON-178: fixed sonar issues

---
+++ src/main/java/org/folio/rest/impl/Path.java
@@ -0,0 +1,18 @@
+package org.folio.rest.impl;
+
+import lombok.Getter;
+
+@Getter
+public enum Path {
+
+  CIRCULATION_REQUESTS_ALLOWED_SERVICE_POINTS_PATH ("/circulation/requests/allowed-service-points"),
+  CIRCULATION_BFF_ALLOWED_SERVICE_POINTS_PATH("/circulation-bff/allowed-service-points"),
+  CIRCULATION_SETTINGS_STORAGE_PATH("/circulation-settings-storage/circulation-settings"),
+  ECS_TLR_SETTINGS_PATH("/tlr/settings");
+
+  Path(String path) {
+    this.path = path;
+  }
+
+  private final String path;
+}
