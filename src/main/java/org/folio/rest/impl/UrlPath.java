dba78f5bc8f617fb44d0b9b14e9a564df1caaacf
Antony_Hruschev <anton.gitgit@gmail.com>
2024-10-09 13:29:38 +0400

    MODPATRON-178: add private constructor to class with constants

--- src/main/java/org/folio/rest/impl/UrlPath.java
+++ src/main/java/org/folio/rest/impl/UrlPath.java
@@ -8,4 +8,6 @@ public class UrlPath {
   public static final String CIRCULATION_SETTINGS_STORAGE_URL_PATH =
     "/circulation-settings-storage/circulation-settings";
   public static final String ECS_TLR_SETTINGS_URL_PATH ="/tlr/settings";
+
+  private UrlPath() {}
 }
