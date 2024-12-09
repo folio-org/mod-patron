14c55f8acbfa0df18a0eb5ce35b6ff5d2b6c7556
Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
2024-12-09 14:57:35 +0200

    MODPATRON-194 Create mediated requests in secure tenant (#170)
    * MODPATRON-194 create med-request in secure tenant
    
    * MODPATRON-194 fix broken tests
    
    * MODPATRON-194 add permissions, new interface
    
    * MODPATRON-194 fix code smell
    
    * MODPATRON-194 add logging
    
    * MODPATRON-194 update system variable checking
    
    * MODPATRON-194 fix code smell
    
    * MODPATRON-194 increase minor interface version
    
    * MODPATRON-194 rename variable in test

--- src/main/java/org/folio/rest/impl/UrlPath.java
+++ src/main/java/org/folio/rest/impl/UrlPath.java
@@ -5,8 +5,6 @@ public class UrlPath {
     "/circulation/requests/allowed-service-points";
   public static final String CIRCULATION_BFF_ALLOWED_SERVICE_POINTS_URL_PATH =
     "/circulation-bff/requests/allowed-service-points";
-  public static final String URL_CIRCULATION_BFF_CREATE_REQUEST =
-    "/circulation-bff/requests";
   public static final String URL_CIRCULATION_CREATE_ITEM_REQUEST =
     "/circulation/requests";
   public static final String URL_CIRCULATION_CREATE_INSTANCE_REQUEST =
@@ -14,6 +12,10 @@ public class UrlPath {
   public static final String CIRCULATION_SETTINGS_STORAGE_URL_PATH =
     "/circulation-settings-storage/circulation-settings";
   public static final String ECS_TLR_SETTINGS_URL_PATH ="/tlr/settings";
+  public static final String CREATE_MEDIATED_REQUEST_URL =
+    "/requests-mediated/mediated-requests";
+  public static final String CIRCULATION_BFF_CREATE_ECS_REQUEST_EXTERNAL =
+    "/circulation-bff/create-ecs-request-external";
 
   private UrlPath() {}
 }
