d5ff09ae053e12c323a4a94a792c9da542db8fdf
Gurleen Kaur <34331959+gurleenkaurbp@users.noreply.github.com>
2024-12-19 18:54:00 +0530

    MODPATRON-202 - Added the put /patron/{externalSystemId} API (#176)
    * MODPATRON-202 - Added the put /staging-user/{externalSystemId} API
    
    * MODPATRON-202 - Added the put /patron/{externalSystemId} API
    
    * MODPATRON-202 - Updated the test cases
    
    * MODPATRON-202 - Updated the test cases
    
    * MODPATRON-202 - Updated the test cases
    
    * MODPATRON-202 - Updated the test cases

--- src/main/java/org/folio/patron/rest/models/ExternalPatronErrorCode.java
+++ src/main/java/org/folio/patron/rest/models/ExternalPatronErrorCode.java
@@ -7,6 +7,7 @@ public enum ExternalPatronErrorCode {
   USER_ACCOUNT_INACTIVE("User account is not active"),
   USER_ALREADY_EXIST("User already exists in the remote patron group"),
   USER_NOT_FOUND("User does not exist"),
+  STAGING_USER_NOT_FOUND("Staging user does not exist"),
   EMAIL_ALREADY_EXIST("User already exist with email provided in payload"),
   INVALID_PATRON_GROUP("User does not belong to the required patron group"),
   PATRON_GROUP_NOT_APPLICABLE("Required Patron group not applicable for user");
