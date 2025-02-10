d82b6504f6ec3cecd34430b9817d99add081f562
Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
2025-02-10 19:18:57 +0200

    MODPATRON-219 Fix IndexOutOfBounds exception for single tenant environment (#188)
    * MODPATRON-219 fix indexOutOfBoundsException
    
    * MODPATRON-219 fix indexOutOfBoundsException
    
    * MODPATRON-219 fix indexOutOfBoundsException
    
    * MODPATRON-219 use primitive return type
    
    * MODPATRON-219 update logging
    
    (cherry picked from commit c4aaecc17859f09f18989a25ba5d8c273d73023d)

--- src/main/java/org/folio/integration/http/ResponseInterpreter.java
+++ src/main/java/org/folio/integration/http/ResponseInterpreter.java
@@ -4,14 +4,18 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 import java.util.concurrent.CompletionException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.patron.rest.exceptions.HttpException;
 
 import io.vertx.core.json.JsonObject;
 
 public class ResponseInterpreter {
+  private static final Logger log = LogManager.getLogger();
   private ResponseInterpreter() {}
 
   public static JsonObject verifyAndExtractBody(Response response) {
+    log.info("verifyAndExtractBody:: statusCode: {}", response.statusCode);
     if (!response.isSuccess()) {
       throw new CompletionException(new HttpException(response.statusCode,
         response.body));
@@ -19,6 +23,7 @@ public class ResponseInterpreter {
 
     // Parsing an empty body to JSON causes an exception
     if (isBlank(response.body)) {
+      log.info("verifyAndExtractBody:: response is empty");
       return null;
     }
     return new JsonObject(response.body);
