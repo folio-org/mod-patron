f1476e73cf7116d27927ee2dcf25337e289b735e
Roman_Barannyk <roman_barannyk@epam.com>
2025-12-24 18:53:58 +0200

    MODPATRON-244 remove code smells

--- src/test/java/org/folio/rest/impl/RequestTypeItemStatusWhiteListTests.java
+++ src/test/java/org/folio/rest/impl/RequestTypeItemStatusWhiteListTests.java
@@ -5,65 +5,65 @@ import static junit.framework.TestCase.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class RequestTypeItemStatusWhiteListTests {
+class RequestTypeItemStatusWhiteListTests {
 
   @Test
-  public void canCreateHoldRequestWhenItemStatusCheckedOut() {
+  void canCreateHoldRequestWhenItemStatusCheckedOut() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.CHECKED_OUT, RequestType.HOLD));
   }
 
   @Test
-  public void canCreateRecallRequestWhenItemStatusCheckedOut() {
+  void canCreateRecallRequestWhenItemStatusCheckedOut() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.CHECKED_OUT, RequestType.RECALL));
   }
 
   @Test
-  public void cannotCreatePagedRequestWhenItemStatusCheckedOut() {
+  void cannotCreatePagedRequestWhenItemStatusCheckedOut() {
     assertFalse(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.CHECKED_OUT, RequestType.PAGE));
   }
 
   @Test
-  public void cannotCreateNoneRequestWhenItemStatusIsAnything() {
+  void cannotCreateNoneRequestWhenItemStatusIsAnything() {
     assertFalse(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.CHECKED_OUT, RequestType.NONE));
   }
 
   @Test
-  public void canCreateHoldRequestWhenItemStatusOnOrder() {
+  void canCreateHoldRequestWhenItemStatusOnOrder() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.ON_ORDER, RequestType.HOLD));
   }
 
   @Test
-  public void canCreateRecallRequestWhenItemStatusOnOrder() {
+  void canCreateRecallRequestWhenItemStatusOnOrder() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.ON_ORDER, RequestType.RECALL));
   }
 
   @Test
-  public void cannotCreatePagedRequestWhenItemStatusOnOrder() {
+  void cannotCreatePagedRequestWhenItemStatusOnOrder() {
     assertFalse(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.ON_ORDER, RequestType.PAGE));
   }
 
   @Test
-  public void canCreateHoldRequestWhenItemStatusInProcess() {
+  void canCreateHoldRequestWhenItemStatusInProcess() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.IN_PROCESS, RequestType.HOLD));
   }
 
   @Test
-  public void canCreateRecallRequestWhenItemStatusInProcess() {
+  void canCreateRecallRequestWhenItemStatusInProcess() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.IN_PROCESS, RequestType.RECALL));
   }
 
   @Test
-  public void cannotCreatePagedRequestWhenItemStatusInProcess() {
+  void cannotCreatePagedRequestWhenItemStatusInProcess() {
     assertFalse(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.IN_PROCESS, RequestType.PAGE));
   }
 
   @Test
-  public void canCreateRecallRequestWhenItemStatusPaged() {
+  void canCreateRecallRequestWhenItemStatusPaged() {
     assertTrue(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.PAGED, RequestType.RECALL));
   }
 
   @Test
-  public void cannotCreatePagedRequestWhenItemStatusIsNone() {
+  void cannotCreatePagedRequestWhenItemStatusIsNone() {
     assertFalse(RequestTypeItemStatusWhiteList.canCreateRequestForItem(ItemStatus.NONE, RequestType.PAGE));
   }
 }
